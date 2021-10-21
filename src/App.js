c4257240c82c781e337fdab0265922f2b235550a
donsro <donsrog@yahoo.com>
2021-10-21 08:35:15 -0400

    Adding icons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faTrash } from '@fortawesome/free-solid-svg-icons'
 import Button from './features/button';
 
 function App() {
   return (
     <div className="container">
-      <h1>Assignment</h1>
+      <h1>Buttons</h1>
+      <h4>Assignment</h4>
       <div className="my-5">
         <ul className="list-unstyled">
           <li className="my-2">
-            <Button>Save as...</Button> <Button disabled>Save as... (disabled)</Button>
+            <Button>Save as <FontAwesomeIcon icon={faSave} /></Button> <Button disabled>Save as (disabled)</Button>
           </li>
           <li className="my-2">
-            <Button variant="secondary">Save as...</Button> <Button variant="secondary" disabled>Save as... (disabled)</Button>
+            <Button>Delete <FontAwesomeIcon icon={faTrash} /></Button> <Button disabled>Delete <FontAwesomeIcon icon={faTrash} /> (disabled)</Button>
+          </li>
+          <li className="my-2">
+            <Button variant="secondary">Save as <FontAwesomeIcon icon={faSave} /></Button> <Button variant="secondary" disabled>Save as <FontAwesomeIcon icon={faSave} /> (disabled)</Button>
           </li>
           <li></li>
           <li></li>
